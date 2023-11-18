8c4fceb1cfc9bf2fe0ca295b8a640bfeaef301ec
BryanS1989 <bryanstevenal@gmail.com>
2023-11-18 19:10:58 +0100

    Create view OrderView

---
+++ src/views/OrderView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import OrderTable from '../components/order/OrderTable.vue';
+</script>
+
+<template>
+	<main class="py-8">
+		<OrderTable></OrderTable>
+	</main>
+</template>
