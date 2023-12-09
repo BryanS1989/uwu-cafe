55163bf058e93b4599286d26600a19d1294ff3b0
BryanS1989 <bryanstevenal@gmail.om>
2023-12-09 16:16:50 +0100

    Import faBoxOpen icon

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ import {
 	faCandyCane,
 	faUtensils,
 	faFileSignature,
+	faBoxOpen,
 } from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
@@ -40,7 +41,8 @@ library.add(
 	faXmark,
 	faCandyCane,
 	faUtensils,
-	faFileSignature
+	faFileSignature,
+	faBoxOpen
 );
 
 const pinia = createPinia();
